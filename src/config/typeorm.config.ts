f1ff8b4a7d66706b55aba830028a09d8ca1e571b
MinhBlues <nevergone1203@gmail.com>
2020-11-28 23:03:29 +0700

    change database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig : TypeOrmModuleOptions ={
     type: 'postgres',
-    host: 'ec2-52-44-55-63.compute-1.amazonaws.com',
+    host: 'ec2-34-237-166-54.compute-1.amazonaws.com',
     port: 5432,
-    username: 'epregmgnospvqo',
-    password: '60841a6ee37bc2bd0088c1b71b9d45b662005f8d5fd0c5a8da70e1eec140c2df',
-    database: 'dcll2a1qcp15of',
+    username: 'uaglsoddmldlae',
+    password: '59b6ff369371c2b8f2e7fa3f948c5278cfa6099579d6db2908067ead07afe506',
+    database: 'dgtf9lms1205k',
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
     ssl: { rejectUnauthorized: false }
